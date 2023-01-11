34d79cd0270153a42b798d5edf90e8f1b42bdd67
Jakub Ledwon <leedwon12@gmail.com>
2023-01-11 20:30:38 +0100

    add method for getting distance between cities

--- tests/unit/parser/test_graph_parser.py
+++ tests/unit/parser/test_graph_parser.py
@@ -1,4 +1,5 @@
 import pytest
+from vehicle_routing_problem.graph import Graph
 from vehicle_routing_problem.parser import graph_parser
 
 
@@ -12,6 +13,28 @@ def cities_count() -> int:
     return 4
 
 
+@pytest.fixture
+def city() -> str:
+    return 'Krakow'
+
+
+@pytest.fixture
+def other_city() -> str:
+    return 'Katowice'
+
+
+@pytest.fixture
+def distance() -> float:
+    return 123.0
+
+
+@pytest.fixture
+def graph(city: str, other_city: str, distance: float) -> Graph:
+    graph = Graph()
+    graph.add_edge(city1=city, city2=other_city, distance=distance)
+    return graph
+
+
 def test_graph_parser(file_path: str, cities_count: int):
     graph = graph_parser.parse_graph(file_path, cities_count)
     assert graph.adj_matrix == {
@@ -20,3 +43,8 @@ def test_graph_parser(file_path: str, cities_count: int):
         'Chrzanów': {'Białystok': 524, 'Bielsko-Biała': 50.4, 'Gdańsk': 551},
         'Gdańsk': {'Białystok': 429, 'Bielsko-Biała': 587, 'Chrzanów': 551},
     }
+
+
+def test_distance_between_cities(city: str, other_city: str, graph: Graph, distance: float):
+    actual = graph.distance(from_city=city, to_city=other_city)
+    assert actual == distance
