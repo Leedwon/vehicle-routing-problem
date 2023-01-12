3892d2441396db794d0669ed8157ae6b7f877a55
Jakub Ledwon <leedwon12@gmail.com>
2023-01-12 21:12:16 +0100

    Implement method for extracting routes from encoded individual

--- vehicle_routing_problem/vrp_solver.py
+++ vehicle_routing_problem/vrp_solver.py
@@ -1,6 +1,43 @@
 import random
 
 
+def fitness(
+    individual: list,
+    starting_city: str,
+    cars_count: int,
+    cities: list
+) -> float:
+    car_routes = extract_rotues_for_cars(
+        starting_city=starting_city,
+        individual=individual,
+        cars_count=cars_count,
+        cities=cities
+    )
+
+    # TODO car routes are ready now calculate fitness
+    pass
+
+
+def extract_rotues_for_cars(
+    individual: list,
+    starting_city: str,
+    cars_count: int,
+    cities: list,
+) -> list:
+    car_routes = [[starting_city] for _ in range(0, cars_count)]
+
+    cities_to_keys = [(cities[i], key) for i, key in enumerate(individual)]
+
+    sorted_cities_to_keys = sorted(
+        cities_to_keys, key=lambda item: item[1] % 1)
+
+    for city, key in sorted_cities_to_keys:
+        car_index = int(key)
+        car_routes[car_index].append(city)
+
+    return car_routes
+
+
 def create_initial_population(
     cities: list,
     cars_count: int,
