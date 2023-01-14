e36f42af693914497f8e1582a97935e9bf7f807c
Jakub Ledwon <leedwon12@gmail.com>
2023-01-14 19:12:23 +0100

    Add explicit encoding

--- vehicle_routing_problem/parser/demand_parser.py
+++ vehicle_routing_problem/parser/demand_parser.py
@@ -1,6 +1,6 @@
 def parse_cities_demand(filename: str) -> dict:
     cities_to_demand = {}
-    with open(filename) as f:
+    with open(filename, encoding="utf8") as f:
         lines = [line.rstrip() for line in f]
         for line in lines:
             (city, demand) = line.split(",")
